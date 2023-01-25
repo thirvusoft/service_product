e0ddde4ab55763d3845e163cc9b2760f87d1bb71
NandhiniDevi <95607404+Nandhinidevi123@users.noreply.github.com>
2023-01-25 18:03:44 +0530

    Update payment_entry.py

--- service/python/payment_entry.py
+++ service/python/payment_entry.py
@@ -5,26 +5,9 @@ def on_submit(doc,event):
         if(doc.status == "Submitted"):
             
             services=frappe.get_doc("Service",doc.reference_doc)
-            print(services)
             services.advance_amount += doc.paid_amount
             services.save()
-    # if(doc.references):
-       
-    #     a=frappe.get_all("Payment Entry Reference",
-	# 			filters={"parent":doc.name},
-	# 			fields=["reference_doctype","reference_name"])
-    #     for i in a:
-    #         if (i["reference_doctype"]=="Sales Invoice"):
-    #             service_doc=frappe.get_doc("Sales Invoice",i["reference_name"])
-    #             if (service_doc.reference_doc):
-    #                 services=frappe.get_doc("Service",service_doc.reference_doc)
-    #                 services.advance_amount += doc.paid_amount
-    #         services.save(ignore_permissions=True)
 
-
-        # print(a)
-        # print(doc.references)
-        # print(doc.name1)
 def on_canceled(doc,event):
     if(doc.reference_doc and frappe.db.exists("Service", doc.reference_doc)):
             services=frappe.get_doc("Service",doc.reference_doc)
